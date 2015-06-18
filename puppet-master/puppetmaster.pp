964a19061810d71757c470beaf9eecd6775826fa
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-06-18 06:55:08 +0100

    Adding a simple hiera configuration

--- puppet-master/puppetmaster.pp
+++ puppet-master/puppetmaster.pp
@@ -13,4 +13,14 @@ class { 'puppet::master':
   autosign          => '/usr/local/bin/puppet_autosign.rb',
   environmentpath   => '/etc/puppet/environments'
 }
+class { 'puppet::hiera':
+  hiera_data_dir   => '/etc/puppet/environments2',
+  hiera_hierarchy => [
+    '%{::environment}/hiera/role_%{::role}/tier_%{::tier}',
+    '%{::environment}/hiera/role_%{::role}',
+    '%{::environment}/hiera/osfamily/%{::os_family}',
+    '%{::environment}/hiera/virtual/%{::virtual}',
+    '%{::environment}/hiera/common'
+  ],
+}
 
